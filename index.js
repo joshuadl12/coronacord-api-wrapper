7752676ae4de8e586b2a36e659f34fe36a3c44d9
joshuadl12 <47647612+joshuadl12@users.noreply.github.com>
2020-04-11 16:17:18 -0500

    Make it work with myBot Bots

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const fetch = require("node-fetch");
 
 module.exports.all = async () => {
-  const res = await fetch(`http://localhost:9669/api/all`).then(r => r.json());
+  const res = await fetch(`http://localhost:3000/api/all`).then(r => r.json());
   return res;
 }
 
 module.exports.countries = async () => {
-  const res = await fetch(`http://localhost:9669/api/countries`).then(r => r.json());
+  const res = await fetch(`http://localhost:3000/api/countries`).then(r => r.json());
   return res;
 };
 
 module.exports.states = async () => {
-  const res = await fetch("http://localhost:9669/api/states").then(r => r.json());
+  const res = await fetch("http://localhost:3000/api/states").then(r => r.json());
   return res;
 };
