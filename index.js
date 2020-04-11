78e69ed2b788cf1b8e75a91becf653c648f2d5e8
joshuadl12 <47647612+joshuadl12@users.noreply.github.com>
2020-04-11 16:27:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const fetch = require("node-fetch");
 
 module.exports.all = async () => {
-  const res = await fetch(`http://localhost:3000/api/all`).then(r => r.json());
+  const res = await fetch(`http://localhost:3000/api/covid/all`).then(r => r.json());
   return res;
 }
 
 module.exports.countries = async () => {
-  const res = await fetch(`http://localhost:3000/api/countries`).then(r => r.json());
+  const res = await fetch(`http://localhost:3000/api/covid/countries`).then(r => r.json());
   return res;
 };
 
 module.exports.states = async () => {
-  const res = await fetch("http://localhost:3000/api/states").then(r => r.json());
+  const res = await fetch("http://localhost:3000/api/covid/states").then(r => r.json());
   return res;
 };
